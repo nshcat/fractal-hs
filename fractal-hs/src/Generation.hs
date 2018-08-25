f92c0a80938b410f28cd547b82fd68448ce2d3e0
nshcat <nshcat@users.noreply.github.com>
2018-08-26 00:21:41 +0200

    Implemented image generation module

---
+++ fractal-hs/src/Generation.hs
@@ -0,0 +1,71 @@
+{-# LANGUAGE DefaultSignatures #-}
+{-# LANGUAGE RankNTypes #-}
+
+-- | A module offering functions that can be used to generate an actual image using a
+-- rendering function.
+module Generation
+    ( Partitionable(..)
+    , generateImage
+    ) where
+    
+import Configuration
+import Data.Array
+import Control.Parallel.Strategies 
+import qualified Codec.Picture  as P
+import GHC.Conc (numCapabilities)
+    
+class Ix a => Partitionable a where
+    partition :: Int -> (a, a) -> [(a, a)]
+
+    default partition :: (Num a) => Int -> (a, a) -> [(a, a)]
+    partition n r@(l,_) = zipWith (\x y -> (x, x+y-1)) starts steps
+        where
+            (span, longerSpans) = rangeSize r `quotRem` n
+            steps = zipWith (+) (replicate (min (rangeSize r) n) (fromIntegral span)) (replicate longerSpans 1 ++ repeat 0)
+            starts = scanl (+) l steps
+            
+instance Partitionable Int
+
+instance (Partitionable a, Partitionable b) => Partitionable (a, b) where
+    partition n ((x0,y0), (x1, y1)) = do
+        xr'@(x0', x1') <- partition n (x0, x1)
+        let n' = n * rangeSize xr' `div` rangeSize (x0, x1)
+        (y0', y1') <- partition n' (y0, y1)
+        return ((x0', y0'), (x1', y1'))
+
+-- | Create an array of given dimensions using given function that maps an index to an element.
+-- This operation is done according to given parallel strategy.        
+mkArrayPar :: (Partitionable i) => Int -> Strategy e -> (i, i) -> (i -> e) -> Array i e
+mkArrayPar n s bounds f = listArray bounds (concat workUnits)
+    where
+        partitions = partition n bounds
+        workUnits  = parMap (evalList s) (map f . range) partitions
+        
+-- | Create an image using given rendering function, using the number of threads specified in the image
+-- settings.
+generateImagePar :: forall a . P.Pixel a  => ImageSettings -> (Int -> Int -> a) -> Int -> Int -> P.Image a
+generateImagePar s f w h = P.generateImage f' w h
+    where
+        bounds  = ((0, 0), (w-1,h-1))
+        pixels  = mkArrayPar threads rseq bounds (uncurry f)
+        f'      = curry (pixels !)
+        tc      = imageThreadCount s
+        threads = if tc <= 0 then numCapabilities else tc
+        
+-- | Create an image using given rendering function, using only a single thread.
+generateImageSingle :: forall a . P.Pixel a  => ImageSettings -> (Int -> Int -> a) -> Int -> Int -> P.Image a
+generateImageSingle _ f w h = P.generateImage f w h
+
+
+-- | Generate an image using given rendering function. If enabled by the supplied image settings,
+-- this operation will be parallelized.
+generateImage :: forall a . P.Pixel a => ImageSettings -> (Int -> Int -> a) -> Int -> Int -> P.Image a
+generateImage s@ ImageSettings {imageThreadCount=1} f w h   = generateImageSingle s f w h
+generateImage s f w h                                       = generateImagePar s f w h
+
+        
+        
+    
+        
+
+
